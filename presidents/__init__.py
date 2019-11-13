72462434129b72009164151aa4f33f5d3e8d795b
Christopher Brown <io@henrian.com>
2019-11-12 22:39:00 -0500

    Move date / timezone utilities to 'util' module

--- presidents/__init__.py
+++ presidents/__init__.py
@@ -1,9 +1,5 @@
 import os
 import logging
-from datetime import date
-import pytz
-import dateutil.parser
-import dateutil.tz
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger('presidents')
@@ -15,50 +11,3 @@ here = os.path.dirname(__file__) or os.curdir
 root = os.path.dirname(here)
 
 
-def _iter_tzinfos():
-    excluded_tzname_prefixes = (
-        'Etc/',
-        'Asia/', 'Indian/', 'Australia/', 'Pacific/',
-        'Europe/', 'Atlantic/',
-        'Brazil/',
-        'Africa/',
-        'Antarctica/', 'Arctic/')
-    for name in pytz.all_timezones:
-        if not name.startswith(excluded_tzname_prefixes):
-            yield name, dateutil.tz.gettz(name)
-
-    # tz_aliases is a hack for 2-letter abbreviations
-    tz_aliases = [
-        ('PT', 'US/Pacific'),
-        ('MT', 'US/Mountain'),
-        ('CT', 'US/Central'),
-        ('ET', 'US/Eastern')]
-    for alias, name in tz_aliases:
-        yield alias, dateutil.tz.gettz(name)
-
-tzinfos = dict(_iter_tzinfos())
-
-
-def parse_date(text, default_tzinfo=None):
-    '''
-    Parse the string `text` as a standard Python datetime using the dateutil library,
-    setting its timezone to `default_tzinfo`
-    iff default_tzinfo is provided _and_ `s` does not specify a timezone.
-    '''
-    date_object = dateutil.parser.parse(text, fuzzy=True, tzinfos=tzinfos)
-    if default_tzinfo is not None:
-        logger.debug('Setting timezone for %s to default: %s', date_object, default_tzinfo)
-        date_object = date_object.replace(tzinfo=date_object.tzinfo or default_tzinfo)
-    return date_object
-
-
-def calculate_election_day(inauguration_date):
-    '''
-    Calculate the date of Election Day corresponding to the given `inauguration_date`
-    date or datetime: the Tuesday after the first Monday of the preceding November.
-    '''
-    year = inauguration_date.year - 1
-    november_1 = date(year, 11, 1)
-    # weekday() returns 0 for Monday
-    first_monday = date(year, 11, 1 + (7 - november_1.weekday()))
-    return date(year, 11, first_monday.day + 1)
---
+++ presidents/util.py
@@ -0,0 +1,59 @@
+from datetime import date
+from typing import Optional
+import logging
+
+import dateutil.parser
+import dateutil.tz
+import pytz
+
+logger = logging.getLogger(__name__)
+
+
+def _iter_tzinfos():
+    excluded_tzname_prefixes = (
+        'Etc/',
+        'Asia/', 'Indian/', 'Australia/', 'Pacific/',
+        'Europe/', 'Atlantic/',
+        'Brazil/',
+        'Africa/',
+        'Antarctica/', 'Arctic/')
+    for name in pytz.all_timezones:
+        if not name.startswith(excluded_tzname_prefixes):
+            yield name, dateutil.tz.gettz(name)
+
+    # tz_aliases is a hack for 2-letter abbreviations
+    tz_aliases = [
+        ('PT', 'US/Pacific'),
+        ('MT', 'US/Mountain'),
+        ('CT', 'US/Central'),
+        ('ET', 'US/Eastern')]
+    for alias, name in tz_aliases:
+        yield alias, dateutil.tz.gettz(name)
+
+
+tzinfos = dict(_iter_tzinfos())
+
+
+def parse_date(text: str, default_tzinfo: Optional[str] = None) -> date:
+    '''
+    Parse the string `text` as a standard Python datetime using the dateutil library,
+    setting its timezone to `default_tzinfo`
+    iff default_tzinfo is provided _and_ `s` does not specify a timezone.
+    '''
+    date_instance = dateutil.parser.parse(text, fuzzy=True, tzinfos=tzinfos)
+    if default_tzinfo is not None:
+        logger.debug('Setting timezone for %s to default: %s', date_instance, default_tzinfo)
+        date_instance = date_instance.replace(tzinfo=date_instance.tzinfo or default_tzinfo)
+    return date_instance
+
+
+def calculate_election_day(inauguration_date: date) -> date:
+    '''
+    Calculate the date of Election Day corresponding to the given `inauguration_date`
+    date or datetime: the Tuesday after the first Monday of the preceding November.
+    '''
+    year = inauguration_date.year - 1
+    november_1 = date(year, 11, 1)
+    # weekday() returns 0 for Monday
+    first_monday = date(year, 11, 1 + (7 - november_1.weekday()))
+    return date(year, 11, first_monday.day + 1)
